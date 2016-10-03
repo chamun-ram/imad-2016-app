7d45bc27ec261b4c76df2247a29dc1156984bb06
chamun-ram <ramchamun@gmail.com>
2016-10-03 13:00:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text'
 );
 element.innerHTML = 'New Text';
+var image=document.getElementById('img');
+image.onclick = function(){
+    image.style.marginLeft='100px';
+}
